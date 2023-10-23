eb33fb08eeecb1204fb5b1b8df833fb5d0587954
Carl Kristie <51312271+Carl-kristie@users.noreply.github.com>
2023-10-23 14:38:33 +0000

    made changes

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { TitleLogo } from "./Title"
+import logo from '../../assets/images/flogo.png'
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { RiMenu4Line } from "react-icons/ri"
@@ -19,18 +19,12 @@ const Header = () => {
         <div className='container'>
           <div className='logo'>
             <Link href='/'>
-              <TitleLogo title='creative' caption='7' className='logomin' />
+            
             </Link>
           </div>
           <nav className={open ? "openMenu" : "closeMenu"} onClick={() => setOpen(null)}>
             <Link href='/' className={activeLink == "/" ? "activeLink" : "none"}>
               Home
-            </Link>
-            <Link href='/agency' className={activeLink == "/agency" ? "activeLink" : "none"}>
-              Agency
-            </Link>
-            <Link href='/team' className={activeLink == "/team" ? "activeLink" : "none"}>
-              Team
             </Link>
             <Link href='/services' className={activeLink == "/services" ? "activeLink" : "none"}>
               Services
@@ -44,9 +38,8 @@ const Header = () => {
             <Link href='/contact' className={activeLink == "/contact" ? "activeLink" : "none"}>
               Contact
             </Link>
-            <button className='button-primary'>book a consultation</button>
           </nav>
-          <button onClick={() => setOpen(!open)}>{open ? <AiOutlineClose size={25} /> : <RiMenu4Line size={25} />}</button>
+          <button className="navbtn" onClick={() => setOpen(!open)}>{open ? <AiOutlineClose size={25} /> : <RiMenu4Line size={25} />}</button>
         </div>
       </header>
     </>
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>7Creative Desgin</title>
       </Head>
       <Hero />
     </>
